5a97790a604ae096b2f7078752693c2462dc6420
khan saad <32749921+khansaad1275@users.noreply.github.com>
2020-02-13 16:26:43 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,51 @@
+
+# Termux-Games
+# Coded by: Khansaad1275 (You dont become a coder by just changing the credits)
+# Github: https://github.com/khansaad1275/Termux-Games
+
+
+
+echo "Please Don't copy the Code And Give creadits If you are using this project"
+
+
+clear
+
+
+echo "play Games in Termux by-LearnTermux.tech"
+
+
+apt install ruby && gem install lolcat
+pkg install figlet
+
+figlet bastet | lolcat && echo Installing..................... | lolcat
+pkg install bastet
+
+figlet Pacman | lolcat && echo Installing..................... | lolcat
+pkg install pacman4console
+
+figlet moon-buggy | lolcat && echo Installing..................... | lolcat
+pkg install moon-buggy
+
+figlet invaders | lolcat && echo Installing..................... | lolcat
+pkg install ninvaders
+
+figlet snake | lolcat && echo Installing..................... | lolcat
+pkg install nsnake
+
+figlet Greed | lolcat && echo Installing..................... | lolcat
+pkg install greed
+
+figlet Nethack | lolcat && echo Installing..................... | lolcat
+pkg install nethack
+
+figlet Sudoku | lolcat && echo Installing..................... | lolcat
+pkg install nudoku && apt install nudoku
+
+figlet Hangman | lolcat && echo Installing..................... | lolcat
+pkg install git -y && git clone https://github.com/khansaad1275/HangmanPy.git
+
+figlet Requirements | lolcat && echo Installing..................... | lolcat
+pkg install python -y
+
+echo Visit www.LearnTermux.tech for cool post on Termux. | lolcat
+echo Type ./game.sh to start the Termux-Games   | lolcat
