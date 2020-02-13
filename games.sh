3cd94137889410533a3a2aec3d711e81d2839c29
khan saad <32749921+khansaad1275@users.noreply.github.com>
2020-02-13 21:57:12 +0530

    Update games.sh

--- games.sh
+++ games.sh
@@ -97,6 +97,7 @@ case $option in
 	"0")
 	mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings
 	echo "Please Restart to Auto-adjust the Keys"
+;;
 	*) echo "Invalid Choice !!" | lolcat
 ;;
 esac
